9a3f6b9ee99586f3f83582f47b180b1bcc76c116
Michael Wolff <michael.wolff4@gmx.de>
2024-03-22 16:01:32 +0100

    more efficent coding

--- detector/model_ResNET.py
+++
@@ -1,6 +0,0 @@
-import torchvision.models as models
-
-
-def resnet():
-    model = models.resnet.ResNet(models.resnet.BasicBlock, [2, 2, 2, 2], num_classes=2)
-    return model
--- detector/model_mobileNetV3.py
+++
@@ -1,6 +0,0 @@
-import torchvision.models as models
-
-
-def mobilenet():
-    model = models.mobilenet.mobilenet_v3_small(num_classes=2)
-    return model
